b20f7b13e0b485495790dfda2e89d0081b2d4e03
Lê Vĩnh Huy <150580083+HikariL3@users.noreply.github.com>
2025-09-30 22:53:51 +0700

    Add home UI for development

---
+++ app/src/main/java/com/example/bookstore/ui/orders/OrdersViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.bookstore.ui.orders
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class OrdersViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is order Fragment"
+    }
+    val text: LiveData<String> = _text
+}
